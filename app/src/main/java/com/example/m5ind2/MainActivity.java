216159a4bf32e563b49b55620be3dac3b1a9706f
Oswaldo <oswaldoalvaradolopez@gmail.com>
2023-06-11 21:05:27 -0400

    Agregado con fragmentos dinamicos

--- app/src/main/java/com/example/m5ind2/MainActivity.java
+++ app/src/main/java/com/example/m5ind2/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.m5ind2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.m5ind2.databinding.ActivityMainBinding;
+import com.example.m5ind2.databinding.FragmentStartBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+public ActivityMainBinding mBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+        StartFragment startFragment= new StartFragment();
+
+
+        getSupportFragmentManager().beginTransaction().add(R.id.contenedor, startFragment).commit();
+
     }
 }
--- app/src/main/java/com/example/m5ind2/StartFragment.java
+++ app/src/main/java/com/example/m5ind2/StartFragment.java
@@ -1,8 +1,8 @@
 package com.example.m5ind2;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -12,60 +12,47 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.m5ind2.databinding.ActivityMainBinding;
-import com.example.m5ind2.databinding.FragmentPokemonBinding;
-import com.example.m5ind2.databinding.FragmentQuestionBinding;
 import com.example.m5ind2.databinding.FragmentStartBinding;
 
 
-public class FragmentPokemon extends Fragment {
-    private @NonNull FragmentPokemonBinding mBinding;
-    private FragmentQuestionBinding mBinding2;
+public class StartFragment extends Fragment {
+
+    private FragmentStartBinding mBinding;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-
-    public FragmentPokemon() {
+    public StartFragment() {
         // Required empty public constructor
     }
-
-
-    public static FragmentPokemon newInstance(String param1, String param2) {
-        FragmentPokemon fragment = new FragmentPokemon();
+    public static StartFragment newInstance(String param1, String param2) {
+        StartFragment fragment = new StartFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        mBinding = FragmentPokemonBinding.inflate(inflater,container,false);
+        mBinding = FragmentStartBinding.inflate(inflater,container,false);
 
-        mBinding.regresar.setOnClickListener(new View.OnClickListener(){
+        mBinding.button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 FragmentManager fragmentManager=getActivity().getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.PokemonFragment,StartFragment.newInstance("Hola","Hola"));
+                fragmentTransaction.replace(R.id.starFragment,FragmentPokemon.newInstance("Hola","Hola"));
                 fragmentTransaction.commit();
-                mBinding.regresar.setVisibility(View.GONE);
-                mBinding.imageView.setVisibility(View.GONE);
+
+                mBinding.button.setVisibility(View.GONE);
+
             }
         });
         return mBinding.getRoot();
